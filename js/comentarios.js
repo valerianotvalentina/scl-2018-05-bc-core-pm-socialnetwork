4ef2d4c1106a406ec8609d78ca822a2c70069ffb
antonella <antonella_caroca@hotmail.com>
2018-07-13 18:04:02 -0400

    agrego archivo comentarios.js

--- js/comentarios.js
+++ js/comentarios.js
@@ -1,62 +1,133 @@
-let divDatos = document.getElementById('datos');
-let commentTextArea = document.getElementById('comment');
+// Variables
+const listaTarea = document.getElementById('listaTareas');
 
-let pComment = document.createElement('p');
+// Ejecución eventos
+eventListeners();
 
-function saveComment() { //funcion para guardar comentario
-  //validación inputs, usamos if para que no se imprima un mensaje vacío
-  if((commentTextArea.value ==='')) {
-    alert('no puedes dejar campos vacíos')
-  } else{
-    
-    let comment = commentTextArea.value;
-    localStorage.setItem(comment,comment); //En este punto guardo nombre y comment en mi local storage
-  }
+function eventListeners() {
+     //Cuando se envia el formulario
+     document.getElementById('enviarTarea').addEventListener('click', agregarTarea);//boton crear tarea
+     // Borrar Tweets
+     document.addEventListener('click', borrarTarea);
+     // Contenido cargado
+     document.addEventListener('DOMContentLoaded', localStorageListo);
 }
 
-function printComment() { //funcion para imprimir el mensaje en la pantalla
-  divDatos.innerHTML = '';
-  for(let i=0; i < localStorage.length; i++) { //recorro el local storage 
-  // recorre el localStorage, y guardar cada una de las Key (llave), y luego vamos a llamar a lo que está en el localStorage con getItem
-    let commentName = localStorage.key(i); //Guarda sólo la llave del localStorage en una variable (y guarda sus respectivos valores del localStorage)
-    let comentarioTexto = localStorage.getItem(commentName);
+// Funciones
 
-//manipulación del dom para crear párrafo y mostrar mensaje
-    let singleComment = document.createElement('div');
-    singleComment.className = 'singleComment';
-
-    let pComment = document.createElement('p');
-    let nodeP = document.createTextNode(comentarioTexto);
-    pComment.appendChild(nodeP);
-
-    singleComment.appendChild(pComment);
-
-    divDatos.appendChild(singleComment);
-
-  }
-}
-function deleteComments(){
-
-  const x = confirm("confirme para eliminar su comentario");
-  if(x==true){
-  // Limpiamos todo lo del localStorage con .clear(), recordar el objeto global window.                
-  window.localStorage.clear();
+//Generar elementos del DOM
+function generarDom(mensaje){
   
-  /*commentTextArea.value = '';
-  divDatos.innerHTML = '';*/
-  }else{
-    return false;
-  }
+  // Crear elementos
+  const itemTarea = document.createElement('div');
+  const parrafo = document.createElement('p');
+  const textTarea = document.createTextNode(mensaje);
+  const botonBorrar = document.createElement('button');
+  const textBoton = document.createTextNode('X');
+
+ // añadir clases a elementos
+ parrafo.setAttribute('class', 'd-inline-block')
+ botonBorrar.setAttribute('class','btn btn-dark');
+ botonBorrar.setAttribute('id', 'borrar-tarea');
+
+  //Añade texto al boton
+  botonBorrar.appendChild(textBoton);
+  // añade la tarea al parrafo
+  parrafo.appendChild(textTarea);
+  // añade el tweet a la lista
+  itemTarea.appendChild(parrafo);
+  // añade el botón de borrar al tweet
+  itemTarea.appendChild(botonBorrar);
+  // añade item con tarea y boton a contenedor padre
+ listaTarea.appendChild(itemTarea);
 }
 
-function makeComment(){
-  if(typeof(Storage) !== 'undefined'){
-    saveComment();
-    printComment();
-    
-    commentTextArea.value = '';
-  }else{
-    alert('lo sentimos, el web Storage no tiene soporte :(');
-  }
+// Añadir tareas al documento
+function agregarTarea() {
+     // leer el valor del textarea
+     const tareas = document.getElementById('crearTarea').value;
+     if(tareas ===""){
+       alert("no puede mandar mensajes vacíos");
+     }else{
+     //Crear elementos en el DOM
+     generarDom(tareas)
+     // Añadir a Local Storage
+     agregarTareasLocalStorage(tareas);
+     }
 }
-printComment();
+
+
+// Elimina tarea del DOM
+function borrarTarea(e) {
+    //console.log(e)
+    //console.log(e.target.parentElement)
+    //console.log(e.target.parentElement.innerText)
+    //console.log(e.target.id)
+
+     if(e.target.id === 'borrar-tarea') {
+          e.target.parentElement.remove();
+          borrarTareasLocalStorage(e.target.parentElement.innerText);   
+     }
+}
+
+// Mostrar datos de LocalStorage en la página
+function localStorageListo() {
+     let tareas;
+     tareas = obtenerTareasLocalStorage();
+     tareas.forEach(function(mensaje) {
+          generarDom(mensaje);      
+     });
+}
+
+ //Agrega tweet a local storage
+function agregarTareasLocalStorage(textoTarea) {
+     let tareas;
+
+     tareas = obtenerTareasLocalStorage();
+     // Añadir el nuevo tweet
+     tareas.push(textoTarea);
+     // Convertir de string a arreglo para local storage
+     localStorage.setItem('tareas', JSON.stringify(tareas) );
+     
+}
+
+// Comprobar que haya elementos en localstorage, retorna un arreglo
+function obtenerTareasLocalStorage() {
+     let tareas;
+     // Revisamos los valores de local storage
+     if(localStorage.getItem('tareas') === null) {
+          tareas = []; 
+     } else {
+          tareas = JSON.parse(localStorage.getItem('tareas'));
+          //console.log(tareas);
+     }
+     return tareas;
+}
+
+// Eliminar tweet de Local Storage
+function borrarTareasLocalStorage(tarea) {
+
+  const x = confirm("confirme para eliminar mensaje");
+  if(x === true){
+  
+     let tareas, borrarTarea;
+     // Elimina la X del tweet
+     borrarTarea = tarea.substring(0, tarea.length - 1); //recibe todo el texto de la tarea, más la X y procede a cortar el texto, dejando solo el texto de la tarea, para eliminarla del localStorage
+    //console.log(borrarTarea)
+     tareas = obtenerTareasLocalStorage();
+    //console.log(tareas);
+     tareas.forEach(function(textoArr, index) {
+       //console.log(tarea)
+       //console.log(index)
+          if(borrarTarea === textoArr) {
+               tareas.splice(index, 1);
+          }
+     })
+     localStorage.setItem('tareas', JSON.stringify(tareas));
+     //console.log(tareas)
+
+    }else{
+      return false;
+    }
+
+}
