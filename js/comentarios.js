8f7343d3e171c8ce9993797048455660425043d5
antonella <antonella_caroca@hotmail.com>
2018-07-14 21:54:54 -0400

    agrego modals tipo alert y confirm con detalle en confirm.

--- js/comentarios.js
+++ js/comentarios.js
@@ -26,7 +26,7 @@ function generarDom(mensaje){
   const textBoton = document.createTextNode('X');
 
  // añadir clases a elementos
- parrafo.setAttribute('class', 'd-inline-block')
+ parrafo.setAttribute('class', 'd-inline-block');
  botonBorrar.setAttribute('class','btn btn-dark');
  botonBorrar.setAttribute('id', 'borrar-tarea');
 
@@ -45,14 +45,18 @@ function generarDom(mensaje){
 // Añadir tareas al documento
 function agregarTarea() {
      // leer el valor del textarea
+     
      const tareas = document.getElementById('crearTarea').value;
      if(tareas ===""){
-       alert("no puede mandar mensajes vacíos");
+      $('#myModal').modal()//se llama al modal
+       //alert("no puede mandar mensajes vacíos");
      }else{
      //Crear elementos en el DOM
      generarDom(tareas)
      // Añadir a Local Storage
      agregarTareasLocalStorage(tareas);
+     //limpiar textArea
+     document.getElementById("crearTarea").value="";
      }
 }
 
@@ -106,28 +110,33 @@ function obtenerTareasLocalStorage() {
 
 // Eliminar tweet de Local Storage
 function borrarTareasLocalStorage(tarea) {
+  /* problema al hacer click en cancelar. Se borra el mensaje, pero queda guardado en el local storage. AL
+  hacer click en cancelar, el mensaje debería permanecer intacto en la pantalla, pero se borra. */
 
-  const x = confirm("confirme para eliminar mensaje");
-  if(x === true){
+  $('#myModal2').modal();//se llama al modal
+  document.getElementById("eliminar").addEventListener("click", eliminar);//evento boton eliminar
+  document.getElementById("cancelar").addEventListener("click", cancelar);//evento boton cancelar
+ /* $("#myModal2").on('hidden.bs.modal', function(){  //This event is fired after the modal is closed.*/
   
-     let tareas, borrarTarea;
-     // Elimina la X del tweet
-     borrarTarea = tarea.substring(0, tarea.length - 1); //recibe todo el texto de la tarea, más la X y procede a cortar el texto, dejando solo el texto de la tarea, para eliminarla del localStorage
-    //console.log(borrarTarea)
-     tareas = obtenerTareasLocalStorage();
-    //console.log(tareas);
-     tareas.forEach(function(textoArr, index) {
-       //console.log(tarea)
-       //console.log(index)
-          if(borrarTarea === textoArr) {
-               tareas.splice(index, 1);
-          }
-     })
-     localStorage.setItem('tareas', JSON.stringify(tareas));
-     //console.log(tareas)
+   function eliminar(){//función del evento del botón eliminar
+    let tareas, borrarTarea;
+    // Elimina la X del tweet
+    borrarTarea = tarea.substring(0, tarea.length - 1); //recibe todo el texto de la tarea, más la X y procede a cortar el texto, dejando solo el texto de la tarea, para eliminarla del localStorage
+   //console.log(borrarTarea)
+    tareas = obtenerTareasLocalStorage();
+   //console.log(tareas);
+    tareas.forEach(function(textoArr, index) {
+      //console.log(tarea)
+      //console.log(index)
+         if(borrarTarea === textoArr) {
+              tareas.splice(index, 1);
+         }
+    })
+    localStorage.setItem('tareas', JSON.stringify(tareas));
+    //console.log(tareas)
+  }
+      function cancelar(){//función del evento del botón cancelar
+        return false;
+      }
+  }
 
-    }else{
-      return false;
-    }
-
-}
