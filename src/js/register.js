e0fc8e0f7bada1233a452e0aeba07cde581bd831
Mi Nombre <valeriadroguettl@gmail.com>
2018-07-15 19:20:20 -0400

    diseño registro y login, estilo de mensajes de alerta y bootstrap responsive

--- src/js/register.js
+++ src/js/register.js
@@ -1,37 +1,36 @@
 //Registro
 function registerWithFirebase() {
-	const emailValue = email.value;
-	const passwordValue = password.value;
-	const nameValue= document.getElementById("name").value;
-	
-	firebase.auth().createUserWithEmailAndPassword(emailValue, passwordValue)
-		.then((datos) => {
-			console.log("Usuario creado con éxito");
-			let user = firebase.auth().currentUser;
-			user.updateProfile({ displayName: nameValue})
-				.then(()=>{
-					localStorage.setItem("user", JSON.stringify(user));
-					window.location = "comentarios.html"
-				})
-				.catch((error) => {
-					console.log(error)
-				})
-			
-		})
-		.catch((error) => {
-			let msn = document.getElementById("msnUsuario");
-			if (error.code === "auth/email-already-in-use") {
-				msn.innerHTML = "Usuario registrado";
-			}
+    const emailValue = email.value;
+    const passwordValue = password.value;
+    const nameValue = document.getElementById("name").value;
 
-			if (error.code === "auth/invalid-email") {
-				msn.innerHTML = "Debes ingresar tus datos"
-			}
-			if (error.code === "auth/weak-password") {
-				msn.innerHTML = "Debes ingresar contraseña de al menos 6 caracteres";
-			}
-			console.log("Error de firebase > Código > " + error.code);
-			console.log("Error de firebase > Mensaje > " + error.message);
-		});
+    firebase.auth().createUserWithEmailAndPassword(emailValue, passwordValue)
+        .then((datos) => {
+            console.log("Usuario creado con éxito");
+            let user = firebase.auth().currentUser;
+            user.updateProfile({ displayName: nameValue })
+                .then(() => {
+                    localStorage.setItem("user", JSON.stringify(user));
+                    window.location = "comentarios.html"
+                })
+                .catch((error) => {
+                    console.log(error)
+                })
+
+        })
+        .catch((error) => {
+            let msn = document.getElementById("msnUsuario");
+            if (error.code === "auth/email-already-in-use") {
+                msn.innerHTML = "<span class='error'>Usuario registrado</span>";
+            }
+
+            if (error.code === "auth/invalid-email") {
+                msn.innerHTML = "<span class='error'>Debes ingresar tus datos</span>";
+            }
+            if (error.code === "auth/weak-password") {
+                msn.innerHTML = "<span class='error'>Debes ingresar contraseña de al menos 6 caracteres</span>";
+            }
+            console.log("Error de firebase > Código > " + error.code);
+            console.log("Error de firebase > Mensaje > " + error.message);
+        });
 }
-
