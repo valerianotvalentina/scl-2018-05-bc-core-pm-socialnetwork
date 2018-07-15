b4132393f68534e30e4676ff1b59fad34925a929
Noelia Sabando <lolia_15@hotmail.com>
2018-07-14 21:24:59 -0400

    iteración de funciones registrar y login

--- src/js/login.js
+++ src/js/login.js
@@ -4,18 +4,21 @@ function loginWithFirebase() {
 
 	firebase.auth().signInWithEmailAndPassword(emailValue, passwordValue)
 		.then(() => {
+			window.location = "comentarios.html"
 			console.log("Usuario inició sesión con éxito");
 		})
 		.catch((error) => {
+			let msn= document.getElementById("msnUsuario");
+			if(error.code === "auth/user-not-found"){
+					msn.innerHTML= "Usuario no registrado";
+			}
+			
+			if(error.code==="auth/wrong-password"){
+					msn.innerHTML="Contraseña incorrecta"
+			}
 			console.log("Error de firebase > Código > " + error.code);
 			console.log("Error de firebase > Mensaje > " + error.message);
 		});
 }
-
-function comment() {
-	window.location = "comentarios.html"
-}
-
-
 
 //juana, juana@gmail.com, 1111111
--- src/js/register.js
+++ src/js/register.js
@@ -6,13 +6,20 @@ function registerWithFirebase() {
 	firebase.auth().createUserWithEmailAndPassword(emailValue, passwordValue)
 		.then(() => {
 			console.log("Usuario creado con éxito");
+			window.location = "comentarios.html"
+
 		})
 		.catch((error) => {
+			let msn= document.getElementById("msnUsuario");
+			if(error.code === "auth/email-already-in-use"){
+					msn.innerHTML= "Usuario registrado";
+			}
+			
+			if(error.code==="auth/invalid-email"){
+					msn.innerHTML="Debes ingresar tus datos"
+			}
 			console.log("Error de firebase > Código > " + error.code);
 			console.log("Error de firebase > Mensaje > " + error.message);
 		});
 }
 
-function comment() {
-	window.location = "comentarios.html"
-}
