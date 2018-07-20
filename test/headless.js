1a502282f0e71fc47fe531ea8eca7231114e28e2
Noelia Sabando <lolia_15@hotmail.com>
2018-07-19 20:16:14 -0400

    test user ok

--- test/headless.js
+++ test/headless.js
@@ -1,4 +1,4 @@
-global.window = global;
+
 global.assert = require('chai').assert;
-require('../src/js/perfil');
 require('./perfil.spec.js');
+require('../src/js/perfil');
--- test/perfil.spec.js
+++ test/perfil.spec.js
@@ -1,3 +1,6 @@
+const assert = require('assert');
+const user = require('../src/js/login');
+
 describe("user", ()=>{
   it("deberia ser un objeto", ()=>{
     assert.equal(typeof user, "object");
